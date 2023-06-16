67865d3d29e54c40fdce266faeae66127d7db742
wlisses <128102199+wlissesrd@users.noreply.github.com>
2023-06-16 20:16:30 -0300

    Create alteracaocomsucesso.php

---
+++ alteracaocomsucesso.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sucesso na Alteração de Senha</title>
+    <style>
+        body {
+            background-color: #f3f3f3;
+            font-family: Arial, sans-serif;
+        }
+
+        .container {
+            max-width: 400px;
+            margin: 100px auto;
+            text-align: center;
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        h1 {
+            font-size: 24px;
+            margin-bottom: 20px;
+        }
+
+        p {
+            font-size: 18px;
+            margin-bottom: 20px;
+        }
+
+        a {
+            color: #428bca;
+            text-decoration: none;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Alteração de Senha</h1>
+        <p>A senha foi alterada com sucesso!</p>
+        <br>
+        <a href="login.php">Voltar para a página de login</a>
+    </div>
+</body>
+</html>
